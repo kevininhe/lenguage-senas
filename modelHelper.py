ca4218901a40e428c8113a7997a5649699e55e23
Kevin Infante <kevin.infante2@gmail.com>
2025-02-06 00:07:55 -0500

    🩹 Actualizar el llamado del metodo

--- modelHelper.py
+++ modelHelper.py
@@ -2,8 +2,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
-def predecirSigno(imagenCsv=None):
-    imagenCsv = pd.read_csv("ejemplo/image_1.csv",header=None)
+def predecirSigno(imagenCsv):
     with open('model/modelo_clasificacion_lenguaje.pkl', 'rb') as modeloAr:
         modeloLenguaje = pickle.load(modeloAr)
     return np.argmax(modeloLenguaje.predict(imagenCsv))
